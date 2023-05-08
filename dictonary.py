9349d9bbeb2d8e90cda9436d6cb39a6f49ced349
Soma vamshi <67499614+vamshi7946@users.noreply.github.com>
2023-05-08 20:27:53 +0530

    Delete dictonary.py

--- dictonary.py
+++
@@ -1,8 +0,0 @@
-n=int(input("Enter no. of circles:"))
-dict={}
-for i in range(n):
-    r=float(input("Enter radius of {} circle :".format(i)))
-    circum = 2*3.148*r
-    dict[r]=circum
-print("Dictonary of radius and circumfereence :")
-print(dict)
